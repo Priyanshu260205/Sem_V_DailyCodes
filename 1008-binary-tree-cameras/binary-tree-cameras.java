11a280482716a3cf5e8476178803149c9c55d2af
Priyanshu Goyal <priyanshugoyal2005@gmail.com>
2025-08-13 11:34:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (84.76%) - LeetSync

---
+++ 1008-binary-tree-cameras/binary-tree-cameras.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int Camera = 0;
+    public int minCameraCover(TreeNode root) {
+        int cam_min = minCamera(root);
+        if(cam_min == -1){
+            Camera++;
+        }
+        return Camera++;
+    }
+    public int minCamera(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int left = minCamera(root.left);
+        int right = minCamera(root.right);
+
+        if(left==-1 || right==-1){
+            Camera++;
+            return 1;// Iska matlab Camera set ho gya hai is node par
+        }
+        else if(left==1 || right==1){
+            return 0; //Iska matlab Camera ki fielding already set hai
+        }
+        else{
+            return -1; //Camera is required by the parent
+        }
+
+    }
+}
