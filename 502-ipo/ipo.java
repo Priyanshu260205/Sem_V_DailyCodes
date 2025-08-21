05b559bf2b586a948cba9bc3de253a494dc06e41
Priyanshu Goyal <priyanshugoyal2005@gmail.com>
2025-08-21 12:10:32 +0530

    Time: 104 ms (56.26%) | Memory: 67.3 MB (10.22%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -12,19 +12,20 @@ class Solution {
     public static int helper(int arr[][], int k, int w){
         Arrays.sort(arr, (a,b)->a[0]-b[0]);
         PriorityQueue<int []> pq = new PriorityQueue<>((a,b)->b[1]-a[1]);
-        
-        int i = 0;
-        while (k > 0) {
-            while (i < arr.length && w >= arr[i][0]) {
-                pq.add(arr[i]);
-                i++;
-            }
-            if (pq.isEmpty()) {
-                break; // no feasible project
-            }
-            w += pq.poll()[1]; // do most profitable
-            k--;
+        for(int i=0; i<arr.length;){ 
+            while (i<arr.length && w >= arr[i][0]) { 
+                pq.add(arr[i]); i++; 
+            } 
+            if(pq.isEmpty() || k==0){ 
+                return w; 
+            } 
+            w += pq.poll()[1]; 
+            k--; 
         }
-        return w;
+        while(!pq.isEmpty() && k>0){
+            w += pq.poll()[1];
+            k--;
+        } 
+        return w; 
     }
 }
