289af1d7668a15974bf45f14f441522a047840b8
Priyanshu Goyal <priyanshugoyal2005@gmail.com>
2025-12-23 02:06:14 +0530

    Time: 6 ms (100.00%) | Memory: 44.5 MB (16.88%) - LeetSync

--- 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
+++ 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
@@ -1,22 +1,31 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
-        int n = strs[0].length();
-        int[] f = new int[n];
-        Arrays.fill(f, 1);
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                boolean ok = true;
-                for (String s : strs) {
-                    if (s.charAt(j) > s.charAt(i)) {
-                        ok = false;
-                        break;
-                    }
-                }
-                if (ok) {
-                    f[i] = Math.max(f[i], f[j] + 1);
+        int rows = strs.length;
+        int cols = strs[0].length();
+
+        int[] dp = new int[cols];
+        Arrays.fill(dp, 1);
+
+        int maxKeep = 1;
+
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < i; j++) {
+                if (isValid(strs, j, i, rows)) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
+            maxKeep = Math.max(maxKeep, dp[i]);
         }
-        return n - Arrays.stream(f).max().getAsInt();
+
+        return cols - maxKeep;
+    }
+
+    private boolean isValid(String[] strs, int c1, int c2, int rows) {
+        for (int r = 0; r < rows; r++) {
+            if (strs[r].charAt(c1) > strs[r].charAt(c2)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
