1391377be3a056dfd0d1b28b9a2d4c18b02b70a8
Priyanshu Goyal <priyanshugoyal2005@gmail.com>
2025-12-26 04:00:32 +0530

    Time: 52 ms (67.90%) | Memory: 91.6 MB (6.29%) - LeetSync

---
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long minimumTime(int[] time, int totalTrips) {
+        long left = 1;
+        long right = (long) Arrays.stream(time).min().getAsInt() * totalTrips;
+
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            long trips = 0;
+
+            for (int t : time) {
+                trips += mid / t;
+                if (trips >= totalTrips) break; // avoid overflow
+            }
+
+            if (trips >= totalTrips) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
