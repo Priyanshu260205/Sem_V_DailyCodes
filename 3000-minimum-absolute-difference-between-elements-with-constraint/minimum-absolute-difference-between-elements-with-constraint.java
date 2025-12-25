d01917a7ffd02c7a0c11a99f7b3bbc5326609e25
Priyanshu Goyal <priyanshugoyal2005@gmail.com>
2025-12-26 03:58:49 +0530

    Time: 132 ms (84.44%) | Memory: 106 MB (6.90%) - LeetSync

---
+++ 3000-minimum-absolute-difference-between-elements-with-constraint/minimum-absolute-difference-between-elements-with-constraint.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minAbsoluteDifference(List<Integer> nums, int x) {
+        TreeSet<Integer> set = new TreeSet<>();
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = x; i < nums.size(); i++) {
+            // Add element that is now x distance behind
+            set.add(nums.get(i - x));
+
+            Integer floor = set.floor(nums.get(i));
+            Integer ceil = set.ceiling(nums.get(i));
+
+            if (floor != null)
+                ans = Math.min(ans, Math.abs(nums.get(i) - floor));
+            if (ceil != null)
+                ans = Math.min(ans, Math.abs(nums.get(i) - ceil));
+        }
+
+        return ans;
+    }
+}
