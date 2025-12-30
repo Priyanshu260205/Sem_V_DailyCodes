45d351838791918bd58f88e9106070db91a1b38b
Priyanshu Goyal <priyanshugoyal2005@gmail.com>
2025-12-31 05:09:48 +0530

    Time: 2 ms (80.36%) | Memory: 46.5 MB (63.83%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -25,7 +25,7 @@ class Solution {
     }
 
     public boolean isSafe(char[][] board, int row, int col){
-        for(int i=0; i<board.length; i++){
+        for(int i=0; i<row; i++){
             if(board[i][col] == 'Q') return false;
         }
 
