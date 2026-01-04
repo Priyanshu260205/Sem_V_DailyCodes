c9198f2d2451f0015718e9ecaa7abd09307bce0d
Priyanshu Goyal <priyanshugoyal2005@gmail.com>
2026-01-04 11:57:25 +0530

    Time: 21 ms (25.62%) | Memory: 46.7 MB (18.52%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,26 +1,26 @@
 class Solution {
     public int countSubstrings(String s) {
         int n = s.length();
-        int count = 0;
-        // dp[i][j] will be true if s[i...j] is a palindrome
         boolean[][] dp = new boolean[n][n];
+        int count = 0;
 
-        // We iterate through lengths (len 1 to n) or backward from i
-        // to ensure dp[i+1][j-1] is always calculated before dp[i][j].
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                // Check if characters at boundaries match
-                if (s.charAt(i) == s.charAt(j)) {
-                    // Check if the gap is small (length <= 3) 
-                    // OR if the inner substring is a palindrome
-                    if (j - i <= 2 || dp[i + 1][j - 1]) {
-                        dp[i][j] = true;
-                        count++;
-                    }
+        // length of substring
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+
+                if (len == 1) {
+                    dp[i][j] = true;
+                } else if (len == 2) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                } else {
+                    dp[i][j] =
+                        (s.charAt(i) == s.charAt(j)) && dp[i + 1][j - 1];
                 }
+
+                if (dp[i][j]) count++;
             }
         }
-
         return count;
     }
 }
